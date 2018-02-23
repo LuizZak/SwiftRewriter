5ba908c4d4bd602c50545e0ce7a0737239dd7195
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2018-02-22 23:36:29 -0300

    Adding test specs for intended behavior of prefix operation type resolving

--- Tests/SwiftRewriterLibTests/ExpressionTypeResolverTests.swift
+++ Tests/SwiftRewriterLibTests/ExpressionTypeResolverTests.swift
@@ -13,13 +13,19 @@ class ExpressionTypeResolverTests: XCTestCase {
         assertResolve(.constant(.rawConstant("12.3-a,bc")), expect: .any)
     }
     
-    func assertResolve(_ exp: Expression, expect type: SwiftType, file: String = #file, line: Int = #line) {
+    func testPrefix() {
+        assertResolve(.prefix(op: .subtract, .constant(1)), expect: .int)
+        assertResolve(.prefix(op: .subtract, .constant(1.0)), expect: .float)
+        assertResolve(.prefix(op: .subtract, .constant("abc")), expect: nil)
+    }
+    
+    func assertResolve(_ exp: Expression, expect type: SwiftType?, file: String = #file, line: Int = #line) {
         let resolver = ExpressionTypeResolver()
         
         let result = resolver.visitExpression(exp)
         
         if result.resolvedType != type {
-            recordFailure(withDescription: "Expected expression to resolve as \(result.resolvedType?.description ?? "nil"), but expected \(type)",
+            recordFailure(withDescription: "Expected expression to resolve as \(type?.description ?? "nil"), but expected \(result.resolvedType?.description ?? "nil")",
                           inFile: file, atLine: line, expected: false)
         }
     }
