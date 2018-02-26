557c91b34450e4c89840600e72714ee6f72a08b6
Luiz Silva <luiz.silva@concrete.com.br>
2018-02-26 18:34:37 -0300

    Adding test for recursive member finding

--- Tests/SwiftRewriterLibTests/DefaultUsageAnalyzerTests.swift
+++ Tests/SwiftRewriterLibTests/DefaultUsageAnalyzerTests.swift
@@ -47,6 +47,61 @@ class DefaultUsageAnalyzerTests: XCTestCase {
         XCTAssertEqual(usages.count, 1)
     }
     
+    func testFindMethodUsagesWithRecursiveCall() {
+        let builder = IntentionCollectionBuilder()
+        
+        let body: CompoundStatement = [
+            // B().b().b()
+            .expression(
+                .postfix(
+                    .postfix(
+                        .postfix(
+                            .postfix(
+                                .postfix(
+                                    .identifier("B"),
+                                    .functionCall(arguments: [])
+                                ),
+                                .member("b")
+                            ),
+                            .functionCall(arguments: [])
+                        ),
+                        .member("b")
+                    ),
+                    .functionCall(arguments: [])
+                )
+            )
+        ]
+        
+        builder
+            .createFile(named: "A.m") { file in
+                file
+                    .createClass(withName: "A") { builder in
+                        builder.createVoidMethod(named: "f1") {
+                            return body
+                        }
+                    }
+                    .createClass(withName: "B") { builder in
+                        builder
+                            .createConstructor()
+                            .createMethod(withSignature:
+                                FunctionSignature(name: "b",
+                                                  parameters: [],
+                                                  returnType: .typeName("B")
+                                )
+                        )
+                }
+        }
+        
+        let intentions = builder.build(typeChecked: true)
+        
+        let sut = DefaultUsageAnalyzer(intentions: intentions)
+        let property = intentions.fileIntentions()[0].typeIntentions[1].methods[0]
+        
+        let usages = sut.findUsages(of: property)
+        
+        XCTAssertEqual(usages.count, 2)
+    }
+    
     func testFindPropertyUsages() {
         let builder = IntentionCollectionBuilder()
         
