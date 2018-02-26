6d7b70f64741961a3b80679df025983f6717603d
Luiz Silva <luiz.silva@concrete.com.br>
2018-02-26 18:18:31 -0300

    Adding support for method usage analysis

--- Tests/SwiftRewriterLibTests/DefaultUsageAnalyzerTests.swift
+++ Tests/SwiftRewriterLibTests/DefaultUsageAnalyzerTests.swift
@@ -33,9 +33,50 @@ class DefaultUsageAnalyzerTests: XCTestCase {
                     .createClass(withName: "B") { builder in
                         builder
                             .createConstructor()
-                            .createProperty(named: "b", type: .int)
+                            .createVoidMethod(named: "b")
                     }
             }
+        
+        let intentions = builder.build(typeChecked: true)
+        
+        let sut = DefaultUsageAnalyzer(intentions: intentions)
+        let property = intentions.fileIntentions()[0].typeIntentions[1].methods[0]
+        
+        let usages = sut.findUsages(of: property)
+        
+        XCTAssertEqual(usages.count, 1)
+    }
+    
+    func testFindPropertyUsages() {
+        let builder = IntentionCollectionBuilder()
+        
+        let body: CompoundStatement = [
+            // B().b()
+            .expression(
+                .postfix(
+                    .postfix(
+                        .identifier("B"),
+                        .functionCall(arguments: [])
+                    ),
+                    .member("b")
+                )
+            )
+        ]
+        
+        builder
+            .createFile(named: "A.m") { file in
+                file
+                    .createClass(withName: "A") { builder in
+                        builder.createVoidMethod(named: "f1") {
+                            return body
+                        }
+                    }
+                    .createClass(withName: "B") { builder in
+                        builder
+                            .createConstructor()
+                            .createProperty(named: "b", type: .int)
+                }
+        }
         
         let intentions = builder.build(typeChecked: true)
         
