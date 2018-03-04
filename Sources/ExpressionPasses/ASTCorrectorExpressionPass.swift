2b8203505d8599b40d0d76452c70e66efabc1fdb
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2018-03-04 19:18:35 -0300

    Support for more if-statement transforms on negated booleans

--- Sources/ExpressionPasses/ASTCorrectorExpressionPass.swift
+++ Sources/ExpressionPasses/ASTCorrectorExpressionPass.swift
@@ -12,36 +12,56 @@ public class ASTCorrectorExpressionPass: SyntaxNodeRewriterPass {
         return super.visitIf(stmt)
     }
     
+    public override func visitWhile(_ stmt: WhileStatement) -> Statement {
+        if let corrected = correctToBoolean(stmt.exp) {
+            stmt.exp = corrected
+            
+            notifyChange()
+        }
+        
+        return super.visitWhile(stmt)
+    }
+    
     func correctToBoolean(_ exp: Expression) -> Expression? {
-        guard let type = exp.resolvedType else {
-            return nil
+        func innerHandle(_ exp: Expression, negated: Bool) -> Expression? {
+            guard let type = exp.resolvedType else {
+                return nil
+            }
+            
+            // <Numeric>
+            if context.typeSystem.isNumeric(type.deepUnwrapped) {
+                let outer = exp.binary(op: negated ? .equals : .unequals, rhs: .constant(0))
+                outer.resolvedType = .bool
+                
+                return outer
+            }
+            
+            switch type {
+            // <Bool?> -> <Bool?> == true
+            // !<Bool?> -> <Bool?> != true (negated)
+            case .optional(.bool):
+                let outer = exp.binary(op: negated ? .unequals : .equals, rhs: .constant(true))
+                outer.resolvedType = .bool
+                
+                return outer
+                
+            // <nullable> -> <nullable> != nil
+            // <nullable> -> <nullable> == nil (negated)
+            case .optional(.typeName):
+                let outer = exp.binary(op: negated ? .equals : .unequals, rhs: .constant(.nil))
+                outer.resolvedType = .bool
+                
+                return outer
+                
+            default:
+                return nil
+            }
         }
         
-        // <Numeric>
-        if context.typeSystem.isNumeric(type.deepUnwrapped) {
-            let outer = exp.binary(op: .unequals, rhs: .constant(0))
-            outer.resolvedType = .bool
-            
-            return outer
-        }
-        
-        switch type {
-        // <Bool?> -> <Bool?> == true
-        case .optional(.bool):
-            let outer = exp.binary(op: .equals, rhs: .constant(true))
-            outer.resolvedType = .bool
-            
-            return outer
-            
-        // <nullable> -> <nullable> != nil
-        case .optional(.typeName):
-            let outer = exp.binary(op: .unequals, rhs: .constant(.nil))
-            outer.resolvedType = .bool
-            
-            return outer
-            
-        default:
-            return nil
+        if let unary = exp.asUnary, unary.op == .negate {
+            return innerHandle(unary.exp, negated: true)
+        } else {
+            return innerHandle(exp, negated: false)
         }
     }
 }
--- Tests/ExpressionPassesTests/ASTCorrectorExpressionPassTests.swift
+++ Tests/ExpressionPassesTests/ASTCorrectorExpressionPassTests.swift
@@ -29,6 +29,24 @@ class ASTCorrectorExpressionPassTests: ExpressionPassTestCase {
         ); assertNotifiedChange()
     }
     
+    /// On boolean expressions that are unary-reversed ("!<exp>"), we simply drop
+    /// the unary operator and plug in an inequality to true
+    func testCorrectsIfStatementNegatedBooleanExpressions() {
+        let exp = Expression.unary(op: .negate, Expression.identifier("a").dot("b"))
+        exp.resolvedType = .optional(.bool)
+        
+        let stmt = Statement.if(exp, body: [], else: nil)
+        
+        assertTransform(
+            // if (!a.b) { }
+            statement: stmt,
+            // if (a.b != true) { }
+            into: Statement.if(Expression.identifier("a").dot("b").binary(op: .unequals, rhs: .constant(true)),
+                               body: [],
+                               else: nil)
+        ); assertNotifiedChange()
+    }
+    
     /// In Objective-C, numbers can be used in place of an if expression statement,
     /// and the expression evaluates to true if the number is different from 0
     func testCorrectsIfStatementWithNumericExpression() {
@@ -42,6 +60,24 @@ class ASTCorrectorExpressionPassTests: ExpressionPassTestCase {
             statement: stmt,
             // if (num != 0) { }
             into: Statement.if(exp.binary(op: .unequals, rhs: .constant(0)),
+                               body: [],
+                               else: nil)
+        ); assertNotifiedChange()
+    }
+    
+    /// Negated numeric expressions simply compare as equals to zero.
+    func testCorrectsIfStatementWithNegatedNumericExpression() {
+        let exp = Expression.unary(op: .negate, .identifier("num"))
+        exp.resolvedType = .bool
+        exp.exp.resolvedType = .int
+        
+        let stmt = Statement.if(exp, body: [], else: nil)
+        
+        assertTransform(
+            // if (!num) { }
+            statement: stmt,
+            // if (num == 0) { }
+            into: Statement.if(exp.binary(op: .equals, rhs: .constant(0)),
                                body: [],
                                else: nil)
         ); assertNotifiedChange()
@@ -82,6 +118,25 @@ class ASTCorrectorExpressionPassTests: ExpressionPassTestCase {
         ); assertNotifiedChange()
     }
     
+    /// For otherwise unknown optional expressions, replace check
+    /// with an 'if-nil'-style check
+    func testCorrectsIfStatementWithNegatedNullableValue() {
+        let exp = Expression.unary(op: .negate, .identifier("obj"))
+        exp.resolvedType = .bool
+        exp.exp.resolvedType = .optional(.typeName("NSObject"))
+        
+        let stmt = Statement.if(exp, body: [], else: nil)
+        
+        assertTransform(
+            // if (!obj) { }
+            statement: stmt,
+            // if (obj == nil) { }
+            into: Statement.if(Expression.identifier("obj").binary(op: .equals, rhs: .constant(.nil)),
+                               body: [],
+                               else: nil)
+        ); assertNotifiedChange()
+    }
+    
     /// For unknown typed expressions, perform no attempts to correct.
     func testDontCorrectUnknownExpressions() {
         let exp = Expression.identifier("a").dot("b")
@@ -95,6 +150,90 @@ class ASTCorrectorExpressionPassTests: ExpressionPassTestCase {
             into: Statement.if(Expression.identifier("a").dot("b"),
                                body: [],
                                else: nil)
+        ); assertDidNotNotifyChange()
+    }
+    
+    // MARK: - While statements
+    
+    /// Just like if statements, on while statements the AST Corrector must try
+    /// to correct the expression so that it results in a proper boolean statement.
+    func testCorrectsWhileStatementBooleanExpressions() {
+        let exp = Expression.identifier("a").dot("b")
+        exp.resolvedType = .optional(.bool)
+        
+        let stmt = Statement.while(exp, body: [])
+        
+        assertTransform(
+            // while (a.b) { }
+            statement: stmt,
+            // while (a.b == true) { }
+            into: Statement.while(exp.binary(op: .equals, rhs: .constant(true)),
+                                  body: [])
+        ); assertNotifiedChange()
+    }
+    
+    /// In Objective-C, numbers can be used in place of a while expression statement,
+    /// and the expression evaluates to true if the number is different from 0
+    func testCorrectsWhileStatementWithNumericExpression() {
+        let exp = Expression.identifier("num")
+        exp.resolvedType = .int
+        
+        let stmt = Statement.while(exp, body: [])
+        
+        assertTransform(
+            // while (num) { }
+            statement: stmt,
+            // while (num != 0) { }
+            into: Statement.while(exp.binary(op: .unequals, rhs: .constant(0)),
+                                  body: [])
+        ); assertNotifiedChange()
+    }
+    
+    /// Same as above, but testing an optional value instead.
+    func testCorrectswhileStatementWithNullableNumericExpressions() {
+        let exp = Expression.identifier("num")
+        exp.resolvedType = .optional(.implicitUnwrappedOptional(.int))
+        
+        let stmt = Statement.while(exp, body: [])
+        
+        assertTransform(
+            // while (num) { }
+            statement: stmt,
+            // while (num != 0) { }
+            into: Statement.while(exp.binary(op: .unequals, rhs: .constant(0)),
+                                  body: [])
+        ); assertNotifiedChange()
+    }
+    
+    /// For otherwise unknown optional expressions, replace check
+    /// with an 'while-not-nil'-style check
+    func testCorrectswhileStatementWithNullableValue() {
+        let exp = Expression.identifier("obj")
+        exp.resolvedType = .optional(.typeName("NSObject"))
+        
+        let stmt = Statement.while(exp, body: [])
+        
+        assertTransform(
+            // while (obj) { }
+            statement: stmt,
+            // while (obj != nil) { }
+            into: Statement.while(exp.binary(op: .unequals, rhs: .constant(.nil)),
+                                  body: [])
+        ); assertNotifiedChange()
+    }
+    
+    /// For unknown typed expressions, perform no attempts to correct.
+    func testDontCorrectUnknownExpressionsOnWhile() {
+        let exp = Expression.identifier("a").dot("b")
+        
+        let stmt = Statement.while(exp, body: [])
+        
+        assertTransform(
+            // while (a.b) { }
+            statement: stmt,
+            // while (a.b == true) { }
+            into: Statement.while(Expression.identifier("a").dot("b"),
+                                  body: [])
         ); assertDidNotNotifyChange()
     }
 }
