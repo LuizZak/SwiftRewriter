c93e88939f10750e3b555f7f8e3434ad56256595
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2018-03-15 08:48:00 -0300

    Global definitions for some common math.h functions

--- Sources/GlobalsProviders/CLibGlobalsProviders.swift
+++ Sources/GlobalsProviders/CLibGlobalsProviders.swift
@@ -1,4 +1,10 @@
+import Foundation
+import SwiftAST
 import SwiftRewriterLib
+
+private let cInt = SwiftType.typeName("CInt")
+private let cFloat = SwiftType.typeName("CFloat")
+private let cDouble = SwiftType.typeName("CDouble")
 
 public class CLibGlobalsProviders: GlobalsProvider {
     public init() {
@@ -6,6 +12,57 @@ public class CLibGlobalsProviders: GlobalsProvider {
     }
     
     public func registerDefinitions(on globals: GlobalDefinitions) {
+        let add: (CodeDefinition) -> Void = {
+            globals.recordDefinition($0)
+        }
         
+        add(function(name: "abs", paramTypes: [cInt], returnType: cInt))
+        add(function(name: "fabsf", paramTypes: [cFloat], returnType: cFloat))
+        add(function(name: "fabs", paramTypes: [cDouble], returnType: cDouble))
+        
+        add(function(name: "asinf", paramTypes: [cFloat], returnType: cFloat))
+        add(function(name: "asin", paramTypes: [cDouble], returnType: cDouble))
+        
+        add(function(name: "acosf", paramTypes: [cFloat], returnType: cFloat))
+        add(function(name: "acos", paramTypes: [cDouble], returnType: cDouble))
+        
+        add(function(name: "atanf", paramTypes: [cFloat], returnType: cFloat))
+        add(function(name: "atan", paramTypes: [cDouble], returnType: cDouble))
+        
+        add(function(name: "tanf", paramTypes: [cFloat], returnType: cFloat))
+        add(function(name: "tan", paramTypes: [cDouble], returnType: cDouble))
+        
+        add(function(name: "sinf", paramTypes: [cFloat], returnType: cFloat))
+        add(function(name: "sin", paramTypes: [cDouble], returnType: cDouble))
+        
+        add(function(name: "cosf", paramTypes: [cFloat], returnType: cFloat))
+        add(function(name: "cos", paramTypes: [cDouble], returnType: cDouble))
+        
+        add(function(name: "atan2f", paramTypes: [cFloat, cFloat], returnType: cFloat))
+        add(function(name: "atan2", paramTypes: [cDouble, cDouble], returnType: cDouble))
+        
+        add(function(name: "sqrtf", paramTypes: [cFloat], returnType: cFloat))
+        add(function(name: "sqrt", paramTypes: [cDouble], returnType: cDouble))
+        
+        add(function(name: "ceilf", paramTypes: [cFloat], returnType: cFloat))
+        add(function(name: "ceil", paramTypes: [cDouble], returnType: cDouble))
+        
+        add(function(name: "floorf", paramTypes: [cFloat], returnType: cFloat))
+        add(function(name: "floor", paramTypes: [cDouble], returnType: cDouble))
+    }
+    
+    func function(name: String, paramTypes: [SwiftType], returnType: SwiftType) -> CodeDefinition {
+        let paramSignatures = paramTypes.enumerated().map { (arg) -> ParameterSignature in
+            let (i, type) = arg
+            return ParameterSignature(label: "_", name: "p\(i)", type: type)
+        }
+        
+        let signature =
+            FunctionSignature(name: name, parameters: paramSignatures,
+                              returnType: returnType, isStatic: false)
+        
+        let definition = CodeDefinition(functionSignature: signature, intention: nil)
+        
+        return definition
     }
 }
--- Tests/GlobalsProvidersTests/CLibGlobalsProvidersTests.swift
+++ Tests/GlobalsProvidersTests/CLibGlobalsProvidersTests.swift
@@ -1,6 +1,11 @@
 import XCTest
+import SwiftAST
 import SwiftRewriterLib
 import GlobalsProviders
+
+private let cInt = SwiftType.typeName("CInt")
+private let cFloat = SwiftType.typeName("CFloat")
+private let cDouble = SwiftType.typeName("CDouble")
 
 class CLibGlobalsProvidersTests: XCTestCase {
     var sut: CLibGlobalsProviders!
@@ -13,5 +18,108 @@ class CLibGlobalsProvidersTests: XCTestCase {
         sut = CLibGlobalsProviders()
         
         sut.registerDefinitions(on: globals)
+    }
+    
+    func testDefinedMathLibFunctions() {
+        assertDefined(function: "abs", paramTypes: [cInt], returnType: cInt)
+        assertDefined(function: "fabsf", paramTypes: [cFloat], returnType: cFloat)
+        assertDefined(function: "fabs", paramTypes: [cDouble], returnType: cDouble)
+        
+        assertDefined(function: "asinf", paramTypes: [cFloat], returnType: cFloat)
+        assertDefined(function: "asin", paramTypes: [cDouble], returnType: cDouble)
+        
+        assertDefined(function: "acosf", paramTypes: [cFloat], returnType: cFloat)
+        assertDefined(function: "acos", paramTypes: [cDouble], returnType: cDouble)
+        
+        assertDefined(function: "atanf", paramTypes: [cFloat], returnType: cFloat)
+        assertDefined(function: "atan", paramTypes: [cDouble], returnType: cDouble)
+        
+        assertDefined(function: "tanf", paramTypes: [cFloat], returnType: cFloat)
+        assertDefined(function: "tan", paramTypes: [cDouble], returnType: cDouble)
+        
+        assertDefined(function: "sinf", paramTypes: [cFloat], returnType: cFloat)
+        assertDefined(function: "sin", paramTypes: [cDouble], returnType: cDouble)
+        
+        assertDefined(function: "cosf", paramTypes: [cFloat], returnType: cFloat)
+        assertDefined(function: "cos", paramTypes: [cDouble], returnType: cDouble)
+        
+        assertDefined(function: "atan2f", paramTypes: [cFloat, cFloat], returnType: cFloat)
+        assertDefined(function: "atan2", paramTypes: [cDouble, cDouble], returnType: cDouble)
+        
+        assertDefined(function: "sqrtf", paramTypes: [cFloat], returnType: cFloat)
+        assertDefined(function: "sqrt", paramTypes: [cDouble], returnType: cDouble)
+        
+        assertDefined(function: "ceilf", paramTypes: [cFloat], returnType: cFloat)
+        assertDefined(function: "ceil", paramTypes: [cDouble], returnType: cDouble)
+        
+        assertDefined(function: "floorf", paramTypes: [cFloat], returnType: cFloat)
+        assertDefined(function: "floor", paramTypes: [cDouble], returnType: cDouble)
+    }
+}
+
+extension CLibGlobalsProvidersTests {
+    func assertDefined(variable: String, type: SwiftType, line: Int = #line) {
+        guard let definition = globals.definition(named: variable) else {
+            recordFailure(withDescription: "Expected to find definition \(variable)",
+                          inFile: #file, atLine: line, expected: true)
+            
+            return
+        }
+        
+        guard case let .variable(_, storage) = definition.kind else {
+            recordFailure(
+                withDescription: "Expected to find a variable defined, but found \(definition.kind) instead",
+                inFile: #file, atLine: line, expected: true)
+            return
+        }
+        
+        if storage.type != type {
+            recordFailure(
+                withDescription: "Expected variable to be of type \(type), but found \(storage.type) instead.",
+                inFile: #file, atLine: line, expected: true)
+        }
+    }
+    
+    func assertDefined(function: String, paramTypes: [SwiftType], returnType: SwiftType,
+                       line: Int = #line) {
+        guard let definition = globals.definition(named: function) else {
+            recordFailure(withDescription: "Expected to find definition \(function)",
+                inFile: #file, atLine: line, expected: true)
+            
+            return
+        }
+        
+        guard case let .function(signature) = definition.kind else {
+            recordFailure(
+                withDescription: "Expected to find a function defined, but found \(definition.kind) instead",
+                inFile: #file, atLine: line, expected: true)
+            return
+        }
+        
+        let actualParamTypes = signature.parameters.map { $0.type }
+        
+        if actualParamTypes != paramTypes {
+            let expectedParamsString =
+                "(" + paramTypes.map { $0.description }.joined(separator: ", ") + ")"
+            
+            let actualParamsString =
+                "(" + actualParamTypes.map { $0.description }.joined(separator: ", ") + ")"
+            
+            recordFailure(
+                withDescription: """
+                Expected function to accept \(expectedParamsString), but it \
+                accepts \(actualParamsString) instead.
+                """,
+                inFile: #file, atLine: line, expected: true)
+        }
+        
+        if signature.returnType != returnType {
+            recordFailure(
+                withDescription: """
+                Expected function to return \(returnType), but it returns \
+                \(signature.returnType) instead.
+                """,
+                inFile: #file, atLine: line, expected: true)
+        }
     }
 }
