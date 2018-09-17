52c071c59399f36b9ad1e94fb4a658685bac4478
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2018-09-17 12:30:00 -0300

    Foundation types should technically appear first in compounded type listing

--- Sources/Commons/CompoundedMappingTypeList.swift
+++ Sources/Commons/CompoundedMappingTypeList.swift
@@ -1,12 +1,6 @@
 public enum CompoundedMappingTypeList {
     public static func typeList() -> [CompoundedMappingType] {
         return [
-            UIResponderCompoundType.create(),
-            UIViewCompoundType.create(),
-            UIColorCompoundType.create(),
-            UIGestureRecognizerCompoundType.create(),
-            UILabelCompoundType.create(),
-            UIViewControllerCompoundType.create(),
             FoundationCompoundTypes.nsCalendar.create(),
             FoundationCompoundTypes.nsArray.create(),
             FoundationCompoundTypes.nsMutableArray.create(),
@@ -14,7 +8,13 @@ public enum CompoundedMappingTypeList {
             FoundationCompoundTypes.nsDate.create(),
             FoundationCompoundTypes.nsLocale.create(),
             FoundationCompoundTypes.nsString.create(),
-            FoundationCompoundTypes.nsMutableString.create()
+            FoundationCompoundTypes.nsMutableString.create(),
+            UIResponderCompoundType.create(),
+            UIViewCompoundType.create(),
+            UIColorCompoundType.create(),
+            UIGestureRecognizerCompoundType.create(),
+            UILabelCompoundType.create(),
+            UIViewControllerCompoundType.create(),
         ]
     }
 }
