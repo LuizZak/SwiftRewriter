b354a4d233f0768b8929e4eda4afc8c14c817c94
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2019-06-29 11:47:43 -0300

    Pinning dependencies that have major version 0

--- Package.swift
+++ Package.swift
@@ -21,10 +21,10 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/LuizZak/MiniLexer.git", from: "0.9.0"),
-        .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.3.0"),
+        .package(url: "https://github.com/LuizZak/MiniLexer.git", .exact("0.9.5")),
+        .package(url: "https://github.com/apple/swift-package-manager.git", .exact("0.3.0")),
         .package(url: "https://github.com/LuizZak/antlr4-swift.git", from: "4.0.26"),
-        .package(url: "https://github.com/LuizZak/console.git", from: "0.5.0"),
+        .package(url: "https://github.com/LuizZak/console.git", .exact("0.8.0")),
         .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50000.0"))
     ],
     targets: [
