7f3d31fe20b5bd27e1885f6cf980a3e99f78ecd8
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2019-09-04 17:33:49 -0300

    Makes AccessLevel.open no less visible than AccessLevel.public

--- Tests/SwiftASTTests/AccessLevelTests.swift
+++ Tests/SwiftASTTests/AccessLevelTests.swift
@@ -3,24 +3,34 @@ import SwiftAST
 
 class AccessLevelTests: XCTestCase {
     func testIsMoreVisibleThan() {
+        XCTAssert(AccessLevel.open.isMoreVisible(than: .private))
+        XCTAssert(AccessLevel.open.isMoreVisible(than: .fileprivate))
+        XCTAssert(AccessLevel.open.isMoreVisible(than: .internal))
+        XCTAssertFalse(AccessLevel.open.isMoreVisible(than: .open))
+        XCTAssertFalse(AccessLevel.open.isMoreVisible(than: .public))
+
         XCTAssert(AccessLevel.public.isMoreVisible(than: .private))
         XCTAssert(AccessLevel.public.isMoreVisible(than: .fileprivate))
         XCTAssert(AccessLevel.public.isMoreVisible(than: .internal))
         XCTAssertFalse(AccessLevel.public.isMoreVisible(than: .public))
+        XCTAssertFalse(AccessLevel.public.isMoreVisible(than: .open))
         
         XCTAssert(AccessLevel.internal.isMoreVisible(than: .private))
         XCTAssert(AccessLevel.internal.isMoreVisible(than: .fileprivate))
         XCTAssertFalse(AccessLevel.internal.isMoreVisible(than: .internal))
         XCTAssertFalse(AccessLevel.internal.isMoreVisible(than: .public))
+        XCTAssertFalse(AccessLevel.internal.isMoreVisible(than: .open))
         
         XCTAssert(AccessLevel.fileprivate.isMoreVisible(than: .private))
         XCTAssertFalse(AccessLevel.fileprivate.isMoreVisible(than: .fileprivate))
         XCTAssertFalse(AccessLevel.fileprivate.isMoreVisible(than: .internal))
         XCTAssertFalse(AccessLevel.fileprivate.isMoreVisible(than: .public))
+        XCTAssertFalse(AccessLevel.fileprivate.isMoreVisible(than: .open))
         
         XCTAssertFalse(AccessLevel.private.isMoreVisible(than: .private))
         XCTAssertFalse(AccessLevel.private.isMoreVisible(than: .fileprivate))
         XCTAssertFalse(AccessLevel.private.isMoreVisible(than: .internal))
         XCTAssertFalse(AccessLevel.private.isMoreVisible(than: .public))
+        XCTAssertFalse(AccessLevel.private.isMoreVisible(than: .open))
     }
 }
