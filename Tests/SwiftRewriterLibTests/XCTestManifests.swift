951b5ec804e6b4d92a3308cef8d75591893ea0e1
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2020-01-11 12:19:25 -0300

    Updating Linux test manifests

--- Tests/SwiftRewriterLibTests/XCTestManifests.swift
+++ Tests/SwiftRewriterLibTests/XCTestManifests.swift
@@ -441,6 +441,7 @@ extension SwiftStatementASTReaderTests {
     // to regenerate.
     static let __allTests__SwiftStatementASTReaderTests = [
         ("testAutomaticSwitchFallthrough", testAutomaticSwitchFallthrough),
+        ("testAutotypeWeakDefinition", testAutotypeWeakDefinition),
         ("testBlockDeclaration", testBlockDeclaration),
         ("testDeclaration", testDeclaration),
         ("testDoWhile", testDoWhile),
@@ -451,6 +452,7 @@ extension SwiftStatementASTReaderTests {
         ("testIfStatement", testIfStatement),
         ("testLabeledStatement", testLabeledStatement),
         ("testSwitch", testSwitch),
+        ("testWeakNonPointerDefinition", testWeakNonPointerDefinition),
         ("testWhile", testWhile),
     ]
 }
