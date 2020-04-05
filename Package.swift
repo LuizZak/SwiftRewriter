a6dbb35dad12cc96114f6ea6881f6cd6c268fb29
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2020-04-05 05:19:48 -0300

    Bumping ArgumentParser dep version

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .package(url: "https://github.com/LuizZak/antlr4-swift.git", from: "4.0.29"),
         .package(url: "https://github.com/LuizZak/console.git", .exact("0.8.0")),
         .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50200.0")),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.0.1"))
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.0.4"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define
