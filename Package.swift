c276eca74f9869b5943aec5f45b2f0b09f6ad380
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-06-07 12:54:25 -0300

    Updating code to Swift 5.5

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/LuizZak/MiniLexer.git", .exact("0.10.0")),
         .package(url: "https://github.com/LuizZak/antlr4-swift.git", from: "4.0.32"),
         .package(url: "https://github.com/LuizZak/console.git", .exact("0.8.0")),
-        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50300.0")),
+        .package(url: "https://github.com/apple/swift-syntax.git", .branch("release/5.5")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.3.1"))
     ],
     targets: [
