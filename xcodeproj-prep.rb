ba72059c3b89c9f8b0eed026509a4eb4ed5e883f
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-09-17 14:34:04 -0300

    Fixing xcodeproj-prep.rb to not enable concurrency yet

--- xcodeproj-prep.rb
+++ xcodeproj-prep.rb
@@ -9,20 +9,6 @@ project = Xcodeproj::Project.open(project_path)
 
 configurations = project.build_configurations
 
-puts "Checking if concurrency is enabled..."
-
-configurations.each do |config|
-    # @type [String]
-    other_flags = project.build_settings(config.name)["OTHER_SWIFT_FLAGS"]
-
-    if !other_flags.include?(swift_async_flags)
-        puts "Concurrency is disabled for config #{config.name}, enabling..."
-        
-        other_flags.concat(swift_async_flags)
-        project.build_settings(config.name)["OTHER_SWIFT_FLAGS"] = other_flags
-    end
-end
-
 puts "Done!"
 
 puts "Marking targets #{release_mode_targets} to compile in optimized mode ..."
