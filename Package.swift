460b8c14d7bd01f5704f09eee7092512c5d46cec
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-09-22 12:38:39 -0300

    Updating swift-syntax dependency to 5.5

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/LuizZak/MiniLexer.git", .exact("0.10.0")),
         .package(url: "https://github.com/LuizZak/antlr4-swift.git", from: "4.0.33"),
         .package(url: "https://github.com/LuizZak/console.git", .exact("0.8.0")),
-        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50300.0")),
+        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50500.0")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.3.1"))
     ],
     targets: [
