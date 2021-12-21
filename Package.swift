462567c95d351f6aa195ce49ca8755d55144e561
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-12-21 17:08:37 -0300

    Updating Antlr dependency

--- Package.swift
+++ Package.swift
@@ -210,7 +210,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/LuizZak/MiniLexer.git", .exact("0.10.0")),
-        .package(url: "https://github.com/LuizZak/antlr4-swift.git", .exact("4.0.35")),
+        .package(url: "https://github.com/LuizZak/antlr4-swift.git", .exact("4.1.0")),
         .package(url: "https://github.com/LuizZak/console.git", .exact("0.8.1")),
         .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50500.0")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.3.1")),
