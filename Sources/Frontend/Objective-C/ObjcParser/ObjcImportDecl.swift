be379b829c7d8535245722690cf2a11d4427c428
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2022-12-18 01:51:39 -0300

    Making verbose mode report the line and column of detected #import declarations in Objective-C files

--- Sources/Frontend/Objective-C/ObjcParser/ObjcImportDecl.swift
+++ Sources/Frontend/Objective-C/ObjcParser/ObjcImportDecl.swift
@@ -1,7 +1,10 @@
 import Foundation
+import Utils
 
 public struct ObjcImportDecl {
     public var path: String
+    public var sourceRange: SourceRange
+    
     /// If `true`, indicates `#import` is of `#import <system_header>` variant,
     /// as opposed to `#import "local_file"` variant.
     public var isSystemImport: Bool
--- Sources/SwiftRewriter/Objective-C/ObjectiveCStdoutFileCollectionStepListener.swift
+++ Sources/SwiftRewriter/Objective-C/ObjectiveCStdoutFileCollectionStepListener.swift
@@ -1,14 +1,24 @@
 import Foundation
 import Console
 import ObjectiveCFrontend
+import Utils
 
 class ObjectiveCStdoutFileCollectionStepListener: ObjectiveCFileCollectionStepListener {
     func objectiveCFileCollectionStep(
         _ collectionStep: ObjectiveCFileCollectionStep,
         didAddReferencedFile referencedUrl: URL,
+        sourceRange: SourceRange?,
         forInputFile inputFile: DiskInputFile
     ) {
         
-        print("Found referenced file \(referencedUrl.lastPathComponent) from input file \(inputFile.url.lastPathComponent)")
+        var message = "Found referenced file \(referencedUrl.lastPathComponent) from input file \(inputFile.url.lastPathComponent)"
+        if let location = sourceRange?.start {
+            message += ":\(location.line)"
+            if location.column != 1 {
+                message += ":\(location.column)"
+            }
+        }
+
+        print(message)
     }
 }
